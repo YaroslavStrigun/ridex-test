dcc0a2b919d5c43e3955aa3ae2cd2995c2afd46d
Ярослав <yastrigun@gmail.com>
2018-09-11 20:00:46 +0300

    ADD: Schedule model, migration, seeder

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -9,4 +9,15 @@ class Schedule extends Model
     protected $guarded = [];
 
     public $timestamps = false;
+
+    const MAP_WEEK_DAYS_NAME = [
+        0 => 'Воскресение',
+        1 => 'Понедельник',
+        2 => 'Вторник',
+        3 => 'Среда',
+        4 => 'Четверг',
+        5 => 'Пятница',
+        6 => 'Суббота'
+    ];
+
 }
